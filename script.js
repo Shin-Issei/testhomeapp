789dc95f177a066d943c3c08b384cadd80c8dacf
Jordan <Mrjordancollins@yahoo.com>
2025-06-16 10:30:22 -0400

    Add js file with comments about plan
    Add img folder for background compat purposes

---
+++ script.js
@@ -0,0 +1,16 @@
+// The point? This is the home page that I get instead of just a blank page
+
+/* 
+    So what should be in it?
+    Links to important stuff both static and dynamic. 
+
+
+    The long shot version of thigns is that I'd have to build an entire dashboard
+    app that allows me to:
+
+    - Create new sections and fill them with links.
+    - Store links somewere that I can easily add and delete from
+    - Create custom themes (write something that consumes the picked colors form 
+    a variable or object store or something like that)
+
+*/
